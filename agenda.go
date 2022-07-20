caf97568aff7965f46b17377a73b510157b680c7
Cookie Engineer <cookiengineer@protonmail.com>
2022-07-20 20:53:49 +0200

    :bug: Fix

--- agenda.go
+++ agenda.go
@@ -22,12 +22,12 @@ func main() {
 	http.HandleFunc("/api/tasks/create", server.CreateTask);
 	http.HandleFunc("/api/tasks/modify", server.ModifyTask);
 
+	fmt.Println("Listening on http://localhost:13337");
+
 	err1 := http.ListenAndServe(":13337", nil);
 
 	if err1 != nil {
-		fmt.Println("Sorry, we need port 13337 to be unused.");
-	} else {
-		fmt.Println("Listening on http://localhost:13337/api");
+		fmt.Println("Sorry, network port 13337 needs to be unbound.");
 	}
 
 }
